3bd207ef646f214cbe40001e6cecd1800f5583bb
TomssZv <tomsszvirbulis@gmail.com>
2024-06-30 22:58:51 +0300

    Incorrect value when deleting negative balance transactions

--- src/app/api/transaction/delete/route.ts
+++ src/app/api/transaction/delete/route.ts
@@ -1,41 +1,41 @@
-import prisma from "@/common/utils/prisma";
-import { NextResponse } from "next/server";
+import prisma from '@/common/utils/prisma';
+import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
-  const data = await request.json()
-  const {
-    transactionId
-  } = data;
+  const data = await request.json();
+  const { transactionId } = data;
 
   const deletedTransaction = await prisma.transaction.delete({
     where: {
-      id: transactionId
-    }
-  })
+      id: transactionId,
+    },
+  });
 
   const oldBalance = await prisma.balance.findFirst({
     where: {
-      id: 1
-    }
-  })
+      id: 1,
+    },
+  });
 
   if (!oldBalance) {
-    return NextResponse.json({}, {status: 500})
+    return NextResponse.json({}, { status: 500 });
   }
-
-  const newBalance = deletedTransaction.deducted ? oldBalance.balance + deletedTransaction.amount : oldBalance.balance - deletedTransaction.amount;
-
+  console.log(deletedTransaction);
+  const newBalance = deletedTransaction.deducted
+    ? oldBalance.balance + Math.abs(deletedTransaction.amount)
+    : oldBalance.balance - deletedTransaction.amount;
+  console.log(newBalance);
   const balance = await prisma.balance.update({
     where: {
-      id: 1
+      id: 1,
     },
     data: {
       balance: newBalance,
     },
-  })
+  });
 
   if (!balance) {
-    return NextResponse.json({}, {status: 500})
+    return NextResponse.json({}, { status: 500 });
   }
 
   return NextResponse.json(balance);
