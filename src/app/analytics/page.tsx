cf57b22da6af1357c938fa8a116071f00b5e0ab8
TomssZv <tomsszvirbulis@gmail.com>
2025-02-05 21:05:32 +0200

    Adjust analytics tab

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -2,32 +2,93 @@
 
 import { AnalyticsTab } from "../../components/analyticsTab/analyticsTab"
 import { analyticsTabs } from "@/enums/analyticsTabs"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { AnalyticsTabRoutes } from "../../helpers/analyticsTabRoutes/analyticsTabRoutes"
+import { analyticsApiItem } from "@/common/types/api";
+import TransactionCard from "@/components/transactionCard/TransactionCard"
+import { transaction } from "@/common/types/global"
+
+const tabs = [
+  {
+    id: 1,
+    title: analyticsTabs.Summary
+  },
+  {
+    id: 2,
+    title: analyticsTabs.Category
+  },
+  {
+    id: 3,
+    title: analyticsTabs.Group
+  }
+]
 
 const Page: React.FC = () => {
-  const [selectedTab, setSelectedTab] = useState(analyticsTabs.Summary)
+  const [selectedTab, setSelectedTab] = useState(tabs[0])
+  const [selectedMonth, setSelectedMonth] = useState<string | null>(null)
+  const [analyticsData, setAnalyticsData] = useState<analyticsApiItem[] | transaction[]>([]);
 
-  const tabs = [
-    {
-      id: 1,
-      title: analyticsTabs.Summary
-    },
-    {
-      id: 2,
-      title: analyticsTabs.Category
-    },
-    {
-      id: 3,
-      title: analyticsTabs.Group
+  useEffect(() => {
+    if (selectedMonth && analyticsData.length > 0) {
+      return
     }
-  ]
+
+    const route = AnalyticsTabRoutes(selectedTab.title);
+
+    if (!route?.init) {
+      return;
+    }
+
+    fetch(`${process.env.NEXT_PUBLIC_API_URL + route.init}`)
+      .then(response => response.json())
+      .then(data => setAnalyticsData(data))
+      .catch(err => console.error(err))
+
+  }, [selectedTab, selectedMonth])
+
+  const fetchMonth = async (analyticData: analyticsApiItem) => {
+
+    let preciseId;
+
+    if (selectedTab.title !== analyticsTabs.Summary) {
+      preciseId = selectedTab.title === analyticsTabs.Category ? analyticData.categoryId : analyticData.groupId;
+
+      if (!preciseId) {
+        return
+      }
+    }
+    
+    const route = AnalyticsTabRoutes(selectedTab.title)
+
+    if (!route?.init) {
+      return;
+    }
+
+    const url = `${process.env.NEXT_PUBLIC_API_URL + route.month}/${analyticData.createdAt.substring(0, 4)}/${analyticData.month}${preciseId ? "/" + preciseId : ''}`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    setAnalyticsData(data)
+    setSelectedMonth(analyticData.month)
+  }
 
   return (
     <div>
+      <h1>{selectedMonth ? `Month - ${selectedMonth}` : 'Months'}</h1>
       {tabs.map((tab) => (
-        <button className={`mr-3 ${tab.title === selectedTab && 'underline'}`} key={tab.id} onClick={() => {setSelectedTab(tab.title)}}>{tab.title}</button>
+        <button 
+          className={`mr-3 ${tab.id === selectedTab.id && 'underline'}`} 
+          key={tab.id} 
+          onClick={() => {setSelectedTab(tab), setSelectedMonth(null)}}
+        >
+          {tab.title}
+        </button>
       ))}
-      <AnalyticsTab title={selectedTab} />
+      {selectedMonth ? analyticsData.map(data => {return <div key={data.id}><TransactionCard transaction={data as transaction} /></div>}) : analyticsData.map(data => {
+        return <div onClick={() => {fetchMonth(data as analyticsApiItem)}} key={data.id} className="rounded p-3 border border-black mb-3 pointer">
+          <AnalyticsTab data={data as analyticsApiItem} />
+        </div>
+      })}
     </div>
   )
 }
--- src/enums/analyticsTabs.ts
+++ src/enums/analyticsTabs.ts
@@ -1,14 +1,14 @@
 export enum analyticsTabs {
-  Summary = "Summary",
-  Category = "Category",
-  Group = "Group"
+  Summary = 'Summary',
+  Category = 'Category',
+  Group = 'Group',
 }
 
 export enum analyticsTabApi {
-  SummaryInitFetch = "/analytics/getSummary",
-  SummaryMonthFetch = "/analytics/getMonth",
-  CategoryInitFetch = "/category/getAnalytics",
-  CategoryMonthFetch = "/category/getMonth",
-  GroupInitFetch = "/group/getAnalytics",
-  GroupMonthFetch = "/group/getMonth"
+  SummaryInitFetch = '/analytics/getSummary',
+  SummaryMonthFetch = '/analytics/getMonth',
+  CategoryInitFetch = '/category/getAnalytics',
+  CategoryMonthFetch = '/category/getMonth',
+  GroupInitFetch = '/group/getAnalytics',
+  GroupMonthFetch = '/group/getMonth',
 }
